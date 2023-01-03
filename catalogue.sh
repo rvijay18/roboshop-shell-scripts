dce2f714a11eb98f64c5c7c8d372816d3b2bda7a
vijayaws9775 <reddyvijaykanth24@gmail.com>
2023-01-03 22:53:18 +0530

    Catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -2,25 +2,54 @@ LOG_FILE=/tmp/catalogue
 
 echo "setup Nodejs Repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo "Install Nodejs"
 yum install nodejs -y &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 echo "Add roboshop Application User"
 useradd roboshop &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 echo "Download Catalogue Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 cd /home/roboshop
 
 echo "Extract Catalogue Application Code"
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 mv catalogue-main catalogue
 
@@ -29,17 +58,46 @@ cd /home/roboshop/catalogue
 
 echo "Install Nodejs Dependencies"
 npm install &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 echo "setup catalogue services"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 systemctl daemon-reload &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 systemctl start catalogue &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
+
 
 systemctl enable catalogue &>>$LOG_FILE
-echo status = $?
+if [ $? -eq 0]
+then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
