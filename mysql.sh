7379874c7791a22961fa69e6ec3ca550afddd44b
vijayaws9775 <reddyvijaykanth24@gmail.com>
2023-01-05 15:02:43 +0530

    Script

--- mysql.sh
+++ mysql.sh
@@ -6,7 +6,7 @@ curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/robo
 StatusCheck $?
 
 echo "Disable MySql Default Module to Enable 5.7 MySql"
-dnf module disable mysql &>>$LOG_FILE
+dnf module disable mysql -y &>>$LOG_FILE
 StatusCheck $?
 
 echo "Install MySQl"
