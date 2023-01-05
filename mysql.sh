9f7c53a8dcf8d5cb4499142f98d3c229c7ee26eb
vijayaws9775 <reddyvijaykanth24@gmail.com>
2023-01-05 18:10:15 +0530

    Script

--- mysql.sh
+++ mysql.sh
@@ -21,7 +21,11 @@ StatusCheck $?
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo " SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;" > /tmp/root-pass.sql
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql
+
+echo "change the default password"
+mysql -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
+StatusCheck $?
 
 
 # mysql_secure_installation
